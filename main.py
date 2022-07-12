fb545a0058f28c046b6fcd027814fd79d3852fe2
Gab-serrano <gab.serrano@duocuc.cl>
2022-07-12 17:41:24 -0400

    agrega función anular

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ print("*"*5,"Bienvenido a Casilleros.com","*"*5)
 #Menú
 sw = 0
 while sw == 0:
-    option=(input("¿Qué desea realizar? Elija una opción:\n1. Arrendar un casillero.\n2. Ver casilleros disponibles.\n3. Asignar un nombre a un casillero.\n4. Mostrar casilleros con nombre.\n5. Mostrar total a pagar.\n6. Mostrar total de ventas.\n7. Salir.\n"))
+    option = (input("¿Qué desea realizar? Elija una opción:\n1. Arrendar un casillero.\n2. Ver casilleros disponibles.\n3. Asignar un nombre a un casillero.\n4. Mostrar casilleros con nombre.\n5. Anular casillero arrendado. \n6. Mostrar total a pagar. \n7. Mostrar total de ventas.\n8. Salir.\n"))
     #Opción 1: Arrendar casilleros
     if option == "1":
         pass
@@ -22,13 +22,16 @@ while sw == 0:
     #Opción 4: Mostrar casilleros con nombre
     elif option == "4":
         pass
-    #Opción 5: Mostrar valor a pagar
+    #Opción 5: Anular casillero
     elif option == "5":
         pass
-    #Opción 6: Mostrar total ventas
+    #Opción 6: Mostrar valor a pagar
     elif option == "6":
         pass
-    #Opción 7: Salir
+    #Opción 7: Mostrar total ventas
+    elif option == "7":
+        pass
+    #Opción 8: Salir
     elif option == "7":
         print("Muchas gracias por su visita.\nSaliendo del programa...")
         sw=1
