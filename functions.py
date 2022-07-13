9bff1562c8995f3b76bad16f7d967307a34684ae
Gab-serrano <gab.serrano@duocuc.cl>
2022-07-12 23:51:12 -0400

    update

--- functions.py
+++ functions.py
@@ -4,7 +4,7 @@ import os
 import platform
 
 #Inicializaciones
-
+list_casilleros=['101','102','103','104','105','106','107','108','109']
 #Matrices
 #Mapa de casilleros
 mapa_casilleros = np.empty((3,3), dtype=('U',15))
@@ -14,18 +14,27 @@ for i in range(3):
         mapa_casilleros[i,j]=cont
         cont+=1
 casilleros = mapa_casilleros.copy()
+casill_nombre = mapa_casilleros.copy()
+
 #Casilleros disponibles para arrendar
 def dispon ():
     for i in range(3):
         for j in range(3):
-            if mapa_casilleros[i, j] in "101,102,103,104,105,106,107,108,109":
+            if casilleros[i, j] in list_casilleros:
                 print(mapa_casilleros[i, j])
             else:
                 pass
 
 #Casilleros actuales
-def casill_actual ():
-    print(casilleros)
+def casill_actual():
+    for i in casilleros:
+        for j in i:
+            sw=0
+            while sw == 0:
+                print("|",j,end="| ")
+                sw+=1
+        print("")
+
 #Limpiar pantalla
 def clear():
     if platform.system() == 'Windows':
@@ -33,12 +42,58 @@ def clear():
     else:
         os.system('clear')
 
-#Imprimir casilleros actuales
+#Función sumar monto de venta
 
 
+def ventas(asiento, total_compra=0):
+    if asiento in ['101', '102', '103']:
+        total_compra += 10000
+    elif asiento in ['104', '105', '106']:
+        total_compra += 5000
+    else:
+        total_compra += 3000
+    return total_compra
+                
 # Función arrendar casilleros
+
 def arrendar(num_casillero):
     for i in range (3):
         for j in range(3):
             if casilleros[i, j] == num_casillero:
-                casilleros[i, j] = "Ocupado"
+                casilleros[i, j] = " X "
+            else:
+                pass
+
+#Función nombrar casillero
+def reemplazar_nombre (casillero,nombre):
+    for i in range(3):
+        for j in range(3):
+            if mapa_casilleros[i,j] == casillero:
+                index = np.where(mapa_casilleros == casillero)
+                casill_nombre[tuple(index)] = nombre
+            else:
+                continue
+
+#Casilleros actuales con nombre
+def casill_nom_actual():
+    for i in casill_nombre:
+        for j in i:
+            sw = 0
+            while sw == 0:
+                if j in list_casilleros:
+                    print("|   ------   ",end="|")
+                elif len(j) < 10:
+                    espacio = 10-len(j)
+                    print("|", " "*espacio, j, end="|")
+                sw += 1
+        print("")
+
+#Función anular venta
+
+def anular(casillero):
+    for i in range(3):
+        for j in range(3):
+            if mapa_casilleros[i, j] == casillero:
+                index = np.where(mapa_casilleros == casillero)
+    casilleros[tuple(index)] = mapa_casilleros[tuple(index)]
+    casill_nombre[tuple(index)] = mapa_casilleros[tuple(index)]
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import functions as fn
 import platform
 #Inicializaciones
+total_venta = 0
 
 #Entrada al programa
 print("*"*5,"Bienvenido a Casilleros.com","*"*5)
@@ -9,33 +10,52 @@ print("*"*5,"Bienvenido a Casilleros.com","*"*5)
 #Menú
 sw = 0
 while sw == 0:
-    option = (input("¿Qué desea realizar? Elija una opción:\n1. Arrendar un casillero.\n2. Ver casilleros disponibles.\n3. Asignar un nombre a un casillero.\n4. Mostrar casilleros con nombre.\n5. Anular casillero arrendado. \n6. Mostrar total a pagar. \n7. Mostrar total de ventas.\n8. Salir.\n"))
+    option = input("¿Qué desea realizar? Elija una opción:\n1. Arrendar un casillero.\n2. Ver casilleros disponibles.\n3. Asignar un nombre a un casillero.\n4. Mostrar casilleros con nombre.\n5. Anular casillero arrendado. \n6. Mostrar total a pagar. \n7. Mostrar total de ventas.\n8. Salir.\n")
     #Opción 1: Arrendar casilleros
     if option == "1":
+        fn.clear()
         print("Los casilleros disponibles son los siguientes:")
-        fn.dispon()
-        arr_casill=input("¿Qué casillero desea arrendar?")
+        fn.casill_actual()
+        arr_casill=input("¿Qué casillero desea arrendar?\n")
         fn.arrendar(arr_casill)
-        pass
+        total_venta += fn.ventas(arr_casill)
+        fn.clear()
+
     #Opción 2: Casilleros disponibles
     elif option == "2":
+        fn.clear()
+        print("Los asientos disponibles son los siguientes.")
         fn.casill_actual()
+        skip=input("Presione cualquier tecla para continuar.")
+        fn.clear()
+
     #Opción 3: Asignar nombre a casillero
     elif option == "3":
-        pass
+        fn.clear()
+        casill=input("Ingrese el número de casillero arrendado:\n")
+        nom_casill=input("¿Qué nombre desea asignarle?\n")
+        fn.reemplazar_nombre(casill,nom_casill)
+        fn.clear()
     #Opción 4: Mostrar casilleros con nombre
     elif option == "4":
-        pass
+        fn.clear()
+        print("Estos son los casilleros con nombre:\n")
+        fn.casill_nom_actual()
+        skip = input("Presione cualquier tecla para continuar.")
+        fn.clear()
     #Opción 5: Anular casillero
     elif option == "5":
-        pass
+        fn.clear()
+        casill = input("Ingrese el número de casillero que desea anular:\n")
+        fn.anular(casill)
+        fn.clear()
     #Opción 6: Mostrar valor a pagar
     elif option == "6":
-        pass
-    #Opción 7: Mostrar total ventas
-    elif option == "7":
-        pass
-    #Opción 8: Salir
+        fn.clear()
+        print("Monto total a pagar por el arriendo:\n")
+        print("$",total_venta)
+        skip = input("Presione cualquier tecla para continuar.")
+    #Opción 7: Salir
     elif option == "7":
         print("Muchas gracias por su visita.\nSaliendo del programa...")
         sw=1
